f057ec20b32814326e910a0da20b6e180363457c
don <donovanfung@gmail.com>
2019-03-08 01:26:56 -0800

    added normalized bbox

--- utils/utils.py
+++ utils/utils.py
@@ -17,8 +17,8 @@ def load_bbox(input_file='../milestone_progress/sample_results/Venice-2/Venice-2
           values = line.split(',')
           #data_input.append(a[:])
           data_input.append(list(map(float, values[0:n_values])))
-  bbox = np.asarray(data_input)  
- 
+  bbox = np.asarray(data_input).T  
+  
   return bbox
 
 def create_poly(box):
@@ -39,6 +39,16 @@ def iou_calc(bbox, gtruth):
     iou = calculate_iou(create_poly(bbox), create_poly(gtruth))
     return iou
 
+def normalize_bbox(bbox, img_dir):
+    image = cv2.imread(img_dir)
+    height, width = image.shape[:2]
+    norm_bbox = bbox
+    norm_bbox[0,:] = bbox[0,:]/width
+    norm_bbox[1,:] = bbox[1,:]/height
+    norm_bbox[2,:] = bbox[2,:]/width
+    norm_bbox[3,:] = bbox[3,:]/height
+
+    return norm_bbox
 
 
 
